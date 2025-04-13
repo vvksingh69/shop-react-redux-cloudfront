96a3cffbb2c85bf17bda774f2b0b2ead3ce556df
VvkSingh69 <vvksingh6939@gmail.com>
2025-04-13 16:39:19 +0530

    upload script

---
+++ src/upload.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process');
+const path = require('path');
+
+const distPath = path.resolve(__dirname, '../dist');
+
+const container = '`$web';
+
+execSync(
+  `az storage blob upload-batch -s "${distPath}" -d ${container} --account-name stgsandfrontendnevs001`,
+  {
+    stdio: 'inherit',
+    shell: 'powershell.exe',
+  }
+);
